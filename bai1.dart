434f682fb7ca12a11fcdb96421b486b5666b0466
expertglacier <118358272+expertglacier@users.noreply.github.com>
2022-12-30 13:16:16 +0700

    Create bai1.dart

---
+++ bai1.dart
@@ -0,0 +1,22 @@
+class Solution {
+  int singleNumber(List<int> nums) {
+    nums.sort();
+    print(nums);
+
+    // Kiểm tra phần tử đầu tiên có phải phần tử không trùng không?
+    if (nums.length == 1 || nums[0] != nums[1]) return nums[0];
+
+    // Tìm kiếm phần tử lặp 1 lần từ index 2 đến index (nums.length)-2
+    for (int i = 2; i < nums.length-1; i++) {
+      if (nums[i] == nums[i+1]) {
+        print(nums[i]);
+        i++;
+        continue;
+      } else {
+        return nums[i];
+      }
+    }
+
+    return nums[nums.length-1];
+  }
+}
