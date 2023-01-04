60c3621f936a5161918cd68a293298ab627474e5
expertglacier <118358272+expertglacier@users.noreply.github.com>
2023-01-04 11:35:46 +0700

    Update bai2.dart

--- bai2.dart
+++ bai2.dart
@@ -3,6 +3,7 @@ class Solution {
     int countNumberZero = 0;
     List<int> listZero = [];
 
+    // Xoá các số 0 khỏi List nums, và đếm xem đã xoá bao nhiêu số 0.
     if (nums.length > 1) {
       for (int i = 0; i < nums.length; i++) {
         if (nums[i] == 0) {
@@ -13,6 +14,7 @@ class Solution {
       }
     }
 
+    // Thêm số lượng số 0 đã xoá vào cuối List nums.
     listZero = List.filled(countNumberZero, 0);
     nums.addAll(listZero);
   }
